1d34dcbb04913b35aa8ee1e15fd5339f1874d759
BeetleBumBum <roseinhat@yandex.ru>
2026-06-02 22:58:07 +0300

    feat: XSS (Межсайтовый скриптинг)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,29 +5,40 @@ import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
-import { DB_ADDRESS } from './config'
+import { DB_ADDRESS, ORIGIN_ALLOW } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
+import { sanitizeReq } from './middlewares/sanitize'
 
 const { PORT = 3000 } = process.env
 const app = express()
 
 app.use(cookieParser())
-
-app.use(cors())
-// app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
+// app.use(cors())
+app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
+app.options('*', cors())
+
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; img-src *; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; connect-src 'self' http://localhost:3000 http://localhost:5173"
+  );
+  next();
+})
 
 app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.use(urlencoded({ extended: true }))
 app.use(json())
 
-app.options('*', cors())
+app.use(sanitizeReq);
+
 app.use(routes)
 app.use(errors())
 app.use(errorHandler)
+
 
 // eslint-disable-next-line no-console
 
---
+++ backend/src/middlewares/sanitize.ts
@@ -0,0 +1,53 @@
+import sanitizeHtml from 'sanitize-html'
+
+export const clean = (dirty: string): string => {
+    if (!dirty) return '';
+
+    return sanitizeHtml(dirty, {
+        allowedTags: [
+            'b',
+            'i',
+            'strong',
+            'a',
+            'p',
+            'br',
+            'ul',
+            'ol',
+            'li',
+            'h1',
+            'h2',
+            'h3',
+            'h4',
+            'h5',
+            'h6',
+        ],
+        allowedAttributes: {
+            a: ['href'],
+            img: [
+                'src',
+                'srcset',
+                'alt',
+                'title',
+                'width',
+                'height',
+                'loading',
+            ],
+        },
+        allowedSchemes: ['http', 'https' ],
+        allowedSchemesByTag: {},
+        allowedSchemesAppliedToAttributes: ['href', 'src', 'cite'],
+        allowProtocolRelative: false,
+    });
+};
+
+// санитизируем все строки
+export const sanitizeReq = (req: any, res: any, next: any) => {
+    if (req.body) {
+        for (let key in req.body) {
+            if (typeof req.body[key] === 'string') {
+                req.body[key] = clean(req.body[key]);
+            }
+        }
+    }
+    next();
+}
